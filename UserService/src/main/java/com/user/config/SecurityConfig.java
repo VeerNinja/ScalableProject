d671a54d10dab7c4ddbc017f3d733deffde90c87
veer <veerprakash20@gmail.com>
2025-10-26 12:46:43 +0530

    Your descriptive commit message

---
+++ UserService/src/main/java/com/user/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+		return httpSecurity.csrf(AbstractHttpConfigurer::disable) // Disable CSRF protection if needed
+				.authorizeHttpRequests(requests -> requests.requestMatchers("/users/**", "/actuator/health").permitAll() // Permit
+						.anyRequest().authenticated() // Require authentication for any other requests
+				).build();
+	}
+}
--- UserService/src/main/java/com/user/controller/UserController.java
+++ UserService/src/main/java/com/user/controller/UserController.java
@@ -2,6 +2,7 @@ package com.user.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,9 +25,13 @@ public class UserController {
 
 	@Autowired
 	private UserService userSerive;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
 	@PostMapping("/register")
 	public ResponseEntity<String> register(@RequestBody User user) {
+		String pwd = passwordEncoder.encode(user.getPassword());
+		user.setPassword(pwd);
 		status = userSerive.save(user);
 		if (status)
 			return ResponseEntity.ok("User Register succussfully");
